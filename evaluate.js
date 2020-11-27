b1567b6f1789d527b463d2fea3386f0c19a6862d
Milan <milanvodak02@gmail.com>
2020-11-27 09:51:49 +0100

    Move the evaluated script to a separate file

---
+++ evaluate.js
@@ -0,0 +1,14 @@
+document.querySelector('body iframe[name=chat]').contentWindow.addEventListener('message', event => {
+    if (event.data.type == 'ON_MESSAGE') {
+        let msgObject = event.data.message;
+        console.log('Message received:', msgObject.channel, msgObject.args);
+        let msg;
+
+        if (msgObject.channel.includes('chat')) msg = '**' + msgObject.args[0] + msgObject.args[2] + msgObject.args[3] + '**: ' + msgObject.args[1];
+        else if (msgObject.channel.includes('system')) msg = msgObject.args[0];
+        else msg = msgObject.args.join(' ');
+
+        sendMessage(msg);
+    }
+    else console.log(event.data.type, 'event');
+});
--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const CDP = require('chrome-remote-interface');
 const axios = require('axios');
+const fs = require('fs');
 const { options, webhook } = require('./config.json');
-const script = `
-document.querySelector('body iframe[name=chat]').contentWindow.addEventListener('message', event => {
-  if (event.data.type == 'ON_MESSAGE') {
-      let msgObject = event.data.message;
-      console.log('Message received:', msgObject.channel, msgObject.args);
-      let msg;
-
-      if (msgObject.channel.includes('chat')) msg = '**' + msgObject.args[0] + msgObject.args[2] + msgObject.args[3] + '**: ' + msgObject.args[1];
-      else if (msgObject.channel.includes('system')) msg = msgObject.args[0];
-      else msg = msgObject.args.join(' ');
-
-      sendMessage(msg);
-  }
-  else console.log(event.data.type, 'event');
-});`;
 
 CDP(options, async (client) => {
   console.log('Connected!');
@@ -28,7 +14,7 @@ CDP(options, async (client) => {
 
   await client.Runtime.addBinding({ name: 'sendMessage' }).then(console.log('Message binding created')).catch(e => console.log(e));
 
-  await client.Runtime.evaluate({ expression: script }).then(ret => console.log('Script evaluated', ret)).catch(e => console.log(e));
+  await client.Runtime.evaluate({ expression: fs.readFileSync('./evaluate.js', 'utf-8') }).then(ret => console.log('Script evaluated', ret)).catch(e => console.log(e));
 }).on('error', (err) => {
   console.error(err);
 });
