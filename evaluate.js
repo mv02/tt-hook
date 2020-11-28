423a2f8f32e4886f857985023cf409b0c0e22d0c
Milan <milanvodak02@gmail.com>
2020-11-28 14:07:12 +0100

    Fix the undefined channel exception
    Some messages don't have a channel field set, such as ATC, ban messages or gut knife announcements.

--- evaluate.js
+++ evaluate.js
@@ -6,11 +6,13 @@ document.querySelector('body iframe[name=chat]').contentWindow.addEventListener(
         console.log('Message received:', msgObject.channel, msgObject.args);
         let msg;
 
-        if (msgObject.channel.includes('chat')) msg = '**' + msgObject.args[0] + msgObject.args[2] + msgObject.args[3] + '**: ' + msgObject.args[1];
-        else if (msgObject.channel.includes('radio')) msg = msgObject.args[0] + ': ' + msgObject.args[1];
-        else if (msgObject.channel.includes('system')) msg = msgObject.args[0];
-        else if (/company|faction|dispatch/g.test(msgObject.channel)) msg = '**' + msgObject.args[0] + '**: ' + msgObject.args[1];
-        else msg = msgObject.args.join(' ');
+        if (msgObject.channel != undefined) {
+            if (msgObject.channel.includes('chat')) msg = '**' + msgObject.args[0] + msgObject.args[2] + msgObject.args[3] + '**: ' + msgObject.args[1];
+            else if (msgObject.channel.includes('radio')) msg = msgObject.args[0] + ': ' + msgObject.args[1];
+            else if (msgObject.channel.includes('system')) msg = msgObject.args[0];
+            else if (/company|faction|dispatch/g.test(msgObject.channel)) msg = '**' + msgObject.args[0] + '**: ' + msgObject.args[1];
+            else msg = msgObject.args.join(' ');
+        }
 
         sendMessage(msg);
     }
