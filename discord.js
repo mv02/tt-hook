e9c2b1d3c3d8e68575dc31815a1bbc1ec52f1d94
Milan <milanvodak02@gmail.com>
2021-04-18 16:22:58 +0200

    Add a class to handle Discord webhooks

---
+++ discord.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+module.exports = class DiscordSender {
+    webhooks = {};
+
+    constructor(loggers) {
+        for (let logger of loggers) {
+            this.webhooks[logger.name] = {
+                webhook: logger.webhook,
+                ratelimitRemaining: 5,
+                resetAfter: 0,
+            };
+        }
+    }
+
+    sendMessage(logger, message) {
+        return new Promise(resolve => {
+            this.timeout(this.webhooks[logger].ratelimitRemaining == 0 ? this.webhooks[logger].ratelimitResetAfter + 1000 : 0)
+            .then(() => {
+                axios.post(this.webhooks[logger].webhook, { content: message })
+                .then(res => {
+                    this.webhooks[logger].ratelimitRemaining = res.headers['x-ratelimit-remaining'];
+                    this.webhooks[logger].ratelimitResetAfter = res.headers['x-ratelimit-reset-after'] * 1000;
+                    console.log(`Disc: Executed ${logger} webhook`);
+                    resolve();
+                })
+                .catch(err => {
+                    this.webhooks[logger].ratelimitResetAfter = err.response.data.retry_after;
+                    console.error(`Disc: Error while executing ${logger} webhook: ${err.message}`);
+                    resolve();
+                });
+            });
+        });
+    }
+
+    timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+}
